e7a294a6bf5358fdc4cbe35e06bd5f1df5f574f1
Nihal Chahal <nchahal2002@gmail.com>
2023-12-12 21:20:17 -0600

    made change

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useSelector, useDispatch, connect } from 'react-redux'
 import { clearData, fetchData, incrementId, decrementId, inputId } from './features/dataSlice'
 import { useEffect } from 'react';
 
-function App() {
+function App(props) {
   const dispatch = useDispatch()
   const data = useSelector((state) => state.data)
 
@@ -15,7 +15,7 @@ function App() {
     }
   }
 
-  const mapStateToProps = (state) => ({
+  const mapStateToProps = (state, ownProps) => ({
     objectId: state.data.objectId
   })
 
